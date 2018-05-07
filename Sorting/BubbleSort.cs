4ce21caae92c9f91a271910de25ec4d012591783
Kareem El-Wakeel <elwakeel91@gmail.com>
2018-05-07 18:27:23 +0100

    Cleaned Bubble Sort algorithm

--- Sorting/BubbleSort.cs
+++ Sorting/BubbleSort.cs
@@ -7,32 +7,30 @@ namespace MyAlgorithms
         // Exchange the position of two values in an array
         private static void Exchange<T>(T[] data, int indexA, int indexB)
         {
+            // Store the first value in a new temporary value
             T temp = data[indexA];
+
+            // Set the first value equal to the second value
             data[indexA] = data[indexB];
+
+            // Set the second value equal to the temporary value
             data[indexB] = temp;
         }
 
         // Sorts an integer array using Bubble Sort
         public static void Sort(int[] data)
         {
-            // The index of the two values we'll compare
-            int indexA = 0;
-            int indexB = 0;
-
             // Loop through the array from the start
             for (int j = 0; j < data.Length; j++)
             {
                 // Loop through the array from the end
                 for (int i = data.Length - 1; i > j; i--)
                 {
-                    // Get the index of the two values we'll compare
-                    indexA = i - 1;
-                    indexB = i;
-
-                    // If the first value is bigger than the second value
-                    if (data[indexA] > data[indexB])
+                    // Compare the value at index (i - 1) and (i)
+                    // If the bigger value is on the left
+                    if (data[i - 1] > data[i])
                         // Switch their positions
-                        Exchange(data, indexA, indexB);
+                        Exchange(data, (i - 1), i);
                 }
             }
         }
