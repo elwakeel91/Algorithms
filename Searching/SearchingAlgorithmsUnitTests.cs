ec79bb73c9a7dc2fa30b9f89bea53196013e056c
Kareem El-Wakeel <elwakeel91@gmail.com>
2018-05-09 17:51:32 +0100

    Add algorithms Unit Tests

---
+++ Searching/SearchingAlgorithmsUnitTests.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyAlgorithms.UnitTest
+{
+    [TestClass]
+    public class SearchingAlgorithmsUnitTests
+    {
+        [TestMethod]
+        public void LinearSearch_NumberFound_Two()
+        {
+            // Arrange
+            int[] data = new int[] { 1, 2, 3, 4, 5 };
+            int searchNumber = 3;
+            int result = 0;
+            int expectedResult = 2;
+
+            // Act
+            result = LinearSearch.Search(data, searchNumber);
+
+            // Assert
+            Assert.AreEqual(result, expectedResult);
+        }
+
+        [TestMethod]
+        public void LinearSearch_NumberNotFound_MinusOne()
+        {
+            // Arrange
+            int[] data = new int[] { 1, 2, 3, 4, 5 };
+            int searchNumber = 10;
+            int result = 0;
+            int expectedResult = -1;
+
+            // Act
+            result = LinearSearch.Search(data, searchNumber);
+
+            // Assert
+            Assert.AreEqual(result, expectedResult);
+        }
+
+        [TestMethod]
+        public void BinarySearch_NumberFound_Three()
+        {
+            // Arrange
+            int[] data = new int[] { 1, 2, 3, 4, 5 };
+            int searchNumber = 4;
+            int result = 0;
+            int expectedResult = 3;
+
+            // Act
+            result = BinarySearch.Search(data, searchNumber);
+
+            // Assert
+            Assert.AreEqual(result, expectedResult);
+        }
+
+        [TestMethod]
+        public void BinarySearch_NumberNotFound_MinusOne()
+        {
+            // Arrange
+            int[] data = new int[] { 1, 2, 3, 4, 5 };
+            int searchNumber = 10;
+            int result = 0;
+            int expectedResult = -1;
+
+            // Act
+            result = BinarySearch.Search(data, searchNumber);
+
+            // Assert
+            Assert.AreEqual(result, expectedResult);
+        }
+    }
+}
---
+++ Sorting/SortingAlgorithmsUnitTests.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyAlgorithms.UnitTest
+{
+    [TestClass]
+    public class SortingAlgorithmsUnitTests
+    {
+        [TestMethod]
+        public void BubbleSort_GivenUnsortedArray_SortedArray()
+        {
+            // Arrange
+            int[] data = new int[] { 2, 4, 3, 1 };
+            int[] expectedResult = new int[] { 1, 2, 3, 4 };
+
+            // Act
+            BubbleSort.Sort(data);
+
+            // Assert
+            CollectionAssert.AreEqual(data, expectedResult);
+        }
+
+        [TestMethod]
+        public void SelectionSort_GivenUnsortedArray_SortedArray()
+        {
+            // Arrange
+            int[] data = new int[] { 2, 5, 1, 6 };
+            int[] expectedResult = new int[] { 1, 2, 5, 6 };
+
+            // Act
+            SelectionSort.Sort(data);
+
+            // Assert
+            CollectionAssert.AreEqual(data, expectedResult);
+        }
+
+        [TestMethod]
+        public void InsertionSort_GivenUnsortedArray_SortedArray()
+        {
+            // Arrange
+            int[] data = new int[] { 20, 50, 10, 60 };
+            int[] expectedResult = new int[] { 10, 20, 50, 60 };
+
+            // Act
+            InsertionSort.Sort(data);
+
+            // Assert
+            CollectionAssert.AreEqual(data, expectedResult);
+        }
+
+        [TestMethod]
+        public void HeapSort_GivenUnsortedArray_SortedArray()
+        {
+            // Arrange
+            int[] data = new int[] { 20, 10, 30, 40 };
+            int[] expectedResult = new int[] { 10, 20, 30, 40 };
+
+            // Act
+            HeapSort.Sort(data);
+
+            // Assert
+            CollectionAssert.AreEqual(data, expectedResult);
+        }
+
+        [TestMethod]
+        public void MergeSort_GivenUnsortedArray_SortedArray()
+        {
+            // Arrange
+            int[] data = new int[] { 20, 10, 30, 40 };
+            int[] expectedResult = new int[] { 10, 20, 30, 40 };
+
+            // Act
+            data = MergeSort.Sort(data);
+
+            // Assert
+            CollectionAssert.AreEqual(data, expectedResult);
+        }
+    }
+}
